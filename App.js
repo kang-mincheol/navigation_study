a88739d1b2bc213136db689503b5dc0509a57d3b
kang-mincheol <aaa6315@gmail.com>
2023-03-08 19:45:23 +0900

    navigation param 스터디 중

--- App.js
+++ App.js
@@ -5,16 +5,25 @@
  * @format
  * @flow strict-local
  */
-
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/home';
+import UserScreen from './src/user';
+
+const Stack = createStackNavigator();
 
 class App extends Component {
   render () {
     return (
-      <View>
-        <Text>Hello World</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="User">
+          <Stack.Screen name="Home" component={HomeScreen}/>
+          <Stack.Screen name="User" component={UserScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     )
   }
 }
